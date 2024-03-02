f96d0646660502da63046f632683429b48cc59e0
Nemmen <Aryankammboz@gmail.com>
2024-03-02 16:02:07 +0530

    you

--- cleanScape/App.js
+++ cleanScape/App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-3xl">hey yyuo didi how iyts going </Text>
       <View style={{padding:"20px", width:"100px", height:"100px", backgroundColor:"red"}}>
-        <Text>Hey There</Text>
+        <Text>Hey how There</Text>
       </View>
       <StatusBar style="auto" />
     </View>
